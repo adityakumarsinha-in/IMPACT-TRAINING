40770dff97133087d7b4624477681fdb603207ee
Aditya Kumar Sinha <94775186+adityakumarsinha-in@users.noreply.github.com>
2026-05-14 07:58:38 +0530

    take to number and print

---
+++ DAY_10/sum_return.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class sum_return {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+    }
+
+    int printSum(int a, int b){
+        int sum = a + b;
+
+        return sum;
+    }
+}
+
